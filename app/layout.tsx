6aa3daa89e170a5198a8cdce12fff3fae88e426c
Mryesiller <gokselyesiller@gmail.com>
2024-09-05 23:46:24 +0300

    seo settings updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,64 @@ const geistMono = localFont({
 })
 
 export const metadata: Metadata = {
-  title: "DevTools",
-  description: "A collection of developer tools",
+  title: {
+    default: "DevTools - A Collection of Developer Tools",
+    template: "%s | DevTools",
+  },
+  description:
+    "Comprehensive suite of developer tools to streamline your workflow and boost productivity.",
+  keywords: [
+    "developer tools",
+    "web development",
+    "coding",
+    "productivity",
+    "software engineering",
+  ],
+  authors: [{ name: "Goksel Yesiller" }],
+  creator: "goksel yesiller",
+  publisher: "mryesiller",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://devtools.tools",
+    title: "DevTools - A Collection of Developer Tools",
+    description:
+      "Comprehensive suite of developer tools to streamline your workflow and boost productivity.",
+    siteName: "DevTools",
+    images: [
+      {
+        url: "https://your-devtools-url.com/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "DevTools - A Collection of Developer Tools",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "DevTools - A Collection of Developer Tools",
+    description:
+      "Comprehensive suite of developer tools to streamline your workflow and boost productivity.",
+    images: ["https://your-devtools-url.com/twitter-image.jpg"],
+    creator: "@yourTwitterHandle",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 }
 
 export default function RootLayout({
